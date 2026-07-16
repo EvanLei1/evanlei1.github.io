6676aa8c1d4df72d20a2af6094bbe0af559b9690
Evan Lei <evanlei0512@gmail.com>
2026-07-15 17:36:40 -0700

    Fixed Vue project migration, needs some work

--- src/views/transport/sort-region/NCA/farNorth.vue.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="C:/Users/Public/Documents/GitHub web/evanlei1.github.io/node_modules/@vue/language-core/types/template-helpers.d.ts" />
-/// <reference types="C:/Users/Public/Documents/GitHub web/evanlei1.github.io/node_modules/@vue/language-core/types/props-fallback.d.ts" />
-const __VLS_ctx = {};
-let __VLS_components;
-let __VLS_intrinsics;
-let __VLS_directives;
-const __VLS_export = (await import('vue')).defineComponent({});
-export default {};
